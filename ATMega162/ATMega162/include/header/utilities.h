567f12fc944ef9da79f854d4fa5e6a000686d31d
Even Eide Hansen <eveneha@ntnu.no>
2024-09-25 17:25:23 +0200

    Even vil hjem

---
+++ ATMega162/ATMega162/include/header/utilities.h
@@ -0,0 +1,20 @@
+/*
+ * utilities.h
+ *
+ * Created: 25.09.2024 09:59:14
+ *  Author: eveneha
+ */ 
+
+
+#ifndef UTILITIES_H_
+#define UTILITIES_H_
+
+uint16_t ADDR_BASE			= 0x1000;
+uint16_t ADDR_OFFSET_OLED_C = 0x000;
+uint16_t ADDR_OFFSET_OLED_D = 0x200; 
+uint16_t ADDR_OFFSET_ADC	= 0x400;
+uint16_t ADDR_OFFSET_SRAM	= 0x800; 
+
+
+
+#endif /* UTILITIES_H_ */
